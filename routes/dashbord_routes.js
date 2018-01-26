49ece2dee8433e87825869875104f52eccc9026f
nbnsireesha <nbnsireesha@gmail.com>
2018-01-25 22:50:54 -0600

    added them

--- routes/dashbord_routes.js
+++ routes/dashbord_routes.js
@@ -6,7 +6,7 @@ var db = require('../models');
 router.get('/dashbord', function(req, res){
 	console.log("user id"+req.query.userId);
 	console.log("here in dashbord-routes");
-	res.render('dashbord',{id:req.query.userId});
+	res.render('dashbord', {id:req.query.userId});
 });
 
 module.exports = router;
